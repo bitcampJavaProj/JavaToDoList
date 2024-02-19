c06012c87597829c8061b19b134a4ee728a183d1
서혜리 <hye_dam_2@naver.com>
2024-02-19 18:12:39 +0900

    투두리스트 DTO, DAO 작성

---
+++ JavaToDoList/src/DTO/TeamToDoList.java
@@ -0,0 +1,70 @@
+package DTO;
+
+import java.sql.Date;
+
+public class TeamToDoList {
+	private Integer teamId;
+	private String title;
+	private String content;
+	private Date createDate;
+	private String closedDate;
+	private Integer priority;
+	private Boolean isComplete;
+	private Boolean isDeleted;
+		
+	public Integer getTeamId() {
+		return teamId;
+	}
+	public void setTeamId(Integer teamId) {
+		this.teamId = teamId;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getClosedDate() {
+		return closedDate;
+	}
+	public void setClosedDate(String closedDate) {
+		this.closedDate = closedDate;
+	}
+	public Integer getPriority() {
+		return priority;
+	}
+	public void setPriority(Integer priority) {
+		this.priority = priority;
+	}
+	public Boolean getIsComplete() {
+		return isComplete;
+	}
+	public void setIsComplete(Boolean isComplete) {
+		this.isComplete = isComplete;
+	}
+	public Boolean getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	
+	@Override
+	public String toString() {
+		String str = String.format("할 일: %s\n메모: %s\n생성 날짜: %s\n마감 날짜: %s\n우선 순위: %d\n완료 여부: %d\n삭제 여부: %d\n", 
+									title, content, createDate, closedDate, priority, isComplete, isDeleted);
+		return str;
+	}
+}
