e5f236e1facca815ac2dca44079fa362d3e0a6b3
99duuk <99duuk@gmail.com>
2024-02-21 19:49:26 +0900

    feat : Diary 클래스 생성, gettersetter 추가

--- JavaToDoList/src/DTO/Diary.java
+++ JavaToDoList/src/DTO/Diary.java
@@ -1,5 +1,70 @@
 package DTO;
 
-public class Diary {
+import java.time.LocalDate;
 
+public class Diary {
+	private Integer diaryId;
+	private String title;
+	private String content;
+	private Integer userId;
+	private LocalDate createDate;
+	private Boolean isDeleted;
+
+	public Diary(Integer diaryId2, String title2, String content2, LocalDate createDate2) {
+	}
+
+	public Integer getDiaryId() {
+		return diaryId;
+	}
+
+	public void setDiaryId(Integer diaryId) {
+		this.diaryId = diaryId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public LocalDate getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(LocalDate createDate) {
+		this.createDate = createDate;
+	}
+	
+	public Boolean getIsDeleted() {
+		return isDeleted;
+	}
+
+	public void setIsDeleted(Boolean isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+
+	@Override
+	public String toString() {
+		String str = String.format("제목: %s\n내용: %s\n작성자id: %d\n생성 날짜: %s\n삭제 여부: %b\n", title, content, userId, createDate);
+
+		return str;
+	}
 }
