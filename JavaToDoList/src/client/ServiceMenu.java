85a0d6dd4444d76c45e3c9a46bd4ec02c37b671e
myumyuzzang <myu.myu.zzang@gmail.com>
2024-02-22 13:48:37 +0900

    fix /서비스메뉴 수정

--- JavaToDoList/src/client/ServiceMenu.java
+++ JavaToDoList/src/client/ServiceMenu.java
@@ -1,20 +1,7 @@
 package client;
 
 public interface ServiceMenu {
-	public int JOIN = 1;
-	public int LOGIN = 2;
-	public int LIST_INSERT = 3;
-	public int LIST_DELETE = 4;
-	public int LIST_UPDATE = 5;
-	public int FIND_ALL_LIST = 6;
-	public int FIND_LIST_BY_DUE = 7;
-	public int FIND_COMP_LIST = 8;
-	public int FIND_INCOMP_LIST = 9;
-	public int VOTE_REG = 10;
-	public int VOTE_DEL = 11;
-	public int VOTE_UPDATE = 12;
-	public int FIND_OPEN_VOTE = 13;
-	public int FIND_CLOSED_VOTE = 14;
-	public int FIND_VOTE_RESULT = 15;
-	public int LOGOUT = 16;
+	public int 회원가입 = 1;
+	public int 로그인 = 2;
+	public int 로그아웃 = 3;
 }
