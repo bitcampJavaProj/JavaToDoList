433c9c9561f9db8981c5ea0f232b94889a1820dd
myumyuzzang <myu.myu.zzang@gmail.com>
2024-02-22 10:15:51 +0900

    UserDAO // userID 및 username 변경

--- JavaToDoList/src/DAO/UserDAO.java
+++ JavaToDoList/src/DAO/UserDAO.java
@@ -6,23 +6,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO {
-	public boolean insertUser(Connection connection, String userId, String userName, String password)
+	public boolean insertUser(Connection connection, String userId, String username, String password)
 			throws SQLException {
-		String insertQuery = "INSERT INTO users (userid, username, password) VALUES (?, ?, ?)";
+		String insertQuery = "INSERT INTO users (userId, username, password) VALUES (?, ?, ?)";
 		try (PreparedStatement preparedStatement = connection.prepareStatement(insertQuery)) {
-			preparedStatement.setString(1, userId);
-			preparedStatement.setString(2, userName);
-			preparedStatement.setString(3, password);
+			preparedStatement.setString(1, username);
+			preparedStatement.setString(2, password);
 
 			int rowsAffected = preparedStatement.executeUpdate();
 			return rowsAffected > 0;
 		}
 	}
 
-	public boolean loginUser(Connection connection, String userId, String password) throws SQLException {
-		String loginQuery = "SELECT * FROM users WHERE userid = ? AND password = ?";
+	public boolean loginUser(Connection connection, String username, String password) throws SQLException {
+		String loginQuery = "SELECT * FROM users WHERE userId = ? AND password = ?";
 		try (PreparedStatement preparedStatement = connection.prepareStatement(loginQuery)) {
-			preparedStatement.setString(1, userId);
+			preparedStatement.setString(1, username);
 			preparedStatement.setString(2, password);
 
 			ResultSet resultSet = preparedStatement.executeQuery();
