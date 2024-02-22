5485e81a9462e1adb3f0997253c80c5946263be8
myumyuzzang <myu.myu.zzang@gmail.com>
2024-02-22 13:50:39 +0900

    feat / ServiceMenu2 인터페이스 생성

---
+++ JavaToDoList/src/client/ServiceMenu2.java
@@ -0,0 +1,17 @@
+package client;
+
+public interface ServiceMenu2 {
+	public int 투두리스트_작성 = 1;
+	public int 투두리스트_삭제 = 2;
+	public int 투두리스트_수정 = 3;
+	public int 투두리스트_전체_조회 = 4;
+	public int 투두리스트_완료 = 5;
+	public int 투두리스트_미완료 = 6;
+	public int 다이어리_작성 = 7;
+	public int 다이어리_삭제 = 8;
+	public int 다이어리_수정 = 9;
+	public int 다이어리_전체_조회 = 10;
+	public int 다이어리_특정날짜 = 11;
+	public int FIND_CLOSED_VOTE = 12;
+	public int FIND_VOTE_RESULT = 13;
+}
