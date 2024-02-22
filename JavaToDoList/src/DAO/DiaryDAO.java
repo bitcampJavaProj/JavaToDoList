f1315a3ab5edec592bda18607a725480604b4057
99duuk <99duuk@gmail.com>
2024-02-22 16:35:39 +0900

    fix. DiaryDAO 메서스 static으로 변경, Diary 생성자 수정

--- JavaToDoList/src/DAO/DiaryDAO.java
+++ JavaToDoList/src/DAO/DiaryDAO.java
@@ -13,78 +13,84 @@ import mysql.DBConnection;
 
 public class DiaryDAO {
 
-	private PreparedStatement pstmt;
-	private String sql;
 
-	/**
-	 * @author 권재원 
-	 * 일기 작성
-	 */
-	public int writeDiary(Connection conn, Diary diary) throws Exception {
-		int result = 0;
+   /**
+    * @author 권재원 
+    * 일기 작성
+    */
+   public static int writeDiary(Connection conn, Diary diary, Integer userId) throws Exception {
+      String sql;
+      PreparedStatement pstmt = null;
+      int result = 0;
 
-		try {
-			sql = "INSERT INTO DIARY (title, content, userId)";
-			sql += " VALUES (?, ?, ?, ?)";
+      try {
+         sql = "INSERT INTO DIARY (title, content, userId)";
+         sql += " VALUES (?, ?, ?, ?)";
 
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, diary.getTitle());
-			pstmt.setString(2, diary.getContent());
-			pstmt.setInt(3, diary.getUserId());
+         pstmt = conn.prepareStatement(sql);
+         pstmt.setString(1, diary.getTitle());
+         pstmt.setString(2, diary.getContent());
+         pstmt.setInt(3, userId);
 
-			result = pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pstmt.close();
-		}
+         result = pstmt.executeUpdate();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         pstmt.close();
+      }
 
-		return result;
-	}
+      return result;
+   }
 
-	/**
-	 * @author 권재원 
-	 * 일기 단건 삭제
-	 */
-	public int deleteDiary(Connection conn, Diary diary) throws Exception {
-		int result = 0;
+   /**
+    * @author 권재원 
+    * 일기 단건 삭제
+    */
+   public static int deleteDiary(Connection conn, Diary diary) throws Exception {
+      String sql;
+      PreparedStatement pstmt = null;
+      int result = 0;
 
-		try {
-			sql = "UPDATE DIARY SET isDeleted = 1 WHERE userId = ? AND diaryId = ?";
+      try {
+         sql = "UPDATE DIARY SET isDeleted = 1 WHERE userId = ? AND diaryId = ?";
 
-			pstmt = conn.prepareStatement(sql);
+         pstmt = conn.prepareStatement(sql);
 
-			pstmt.setInt(1, diary.getUserId());
-			pstmt.setInt(2, diary.getDiaryId());
+         pstmt.setInt(1, diary.getUserId());
+         pstmt.setInt(2, diary.getDiaryId());
 
-			result = pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pstmt.close();
-		}
+         result = pstmt.executeUpdate();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         pstmt.close();
+      }
 
-		return result;
-	}
+      return result;
+   }
 
-	/**
-	 * @author 김동우 
-	 * 일기 수정
-	 */
-	public int updateDiary(Connection conn, Diary diary) throws Exception {
-		int result = 0;
-		try {
-			String sql = "UPDATE diary SET title = ?, content = ? WHERER diaryId = ?";
-			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, diary.getTitle());
-			pstmt.setString(2, diary.getContent());
-			result = pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			pstmt.close();
-		}
-		return result;
-	}
+   /**
+    * @author 김동우 
+    * 일기 수정
+    */
+   public static int updateDiary(Connection conn, Diary diary) throws Exception {
+      String sql;
+      PreparedStatement pstmt = null;
+      int result = 0;
+      
+      try {
+         sql = "UPDATE diary SET title = ?, content = ? WHERER diaryId = ?";
+         pstmt = conn.prepareStatement(sql);
+         pstmt.setString(1, diary.getTitle());
+         pstmt.setString(2, diary.getContent());
+         result = pstmt.executeUpdate();
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         pstmt.close();
+      }
+      return result;
+   }
 
+   
 }
