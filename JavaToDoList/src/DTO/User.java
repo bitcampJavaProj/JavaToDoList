2e6e586a251feac0c43ea7975c5059fd95326e76
myumyuzzang <myu.myu.zzang@gmail.com>
2024-02-22 10:38:37 +0900

    fix: user class fixed

--- JavaToDoList/src/DTO/User.java
+++ JavaToDoList/src/DTO/User.java
@@ -2,39 +2,37 @@ package DTO;
 
 public class User {
 
-   private Integer userId;
-   private String username;
-   private String password;
-   
-   
-   public Integer getuserId() {
-      return userId;
-   }
+	private Integer userId;
+	private String username;
+	private String password;
 
-   public void setId(Integer userId) {
-      this.userId = userId;
-   }
-   
-   public String getusername() {
-      return username;
-   }
+	public Integer getUserId() {
+		return userId;
+	}
 
-   public void setusername(String username) {
-      this.username = username;
-   }
-   
-   public String getpassword() {
-      return password;
-   }
-   
-   public void setpassword(String password) {
-      this.password = password;
-   }   
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
 
-   @Override
-   public String toString() {
-      String str = String.format("아이디:%s \n이름:%s \n비밀번호:%s ",
-            userId, username, password );
-      return str;
-   }   
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		String str = String.format("아이디:%s \n이름:%s \n비밀번호:%s ", userId, username, password);
+		return str;
+	}
 }
