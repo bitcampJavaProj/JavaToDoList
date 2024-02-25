b92a30febdfa18cea86fd92c436938142ed554be
서혜리 <hye_dam_2@naver.com>
2024-02-25 18:19:08 +0900

    feat: 투두리스트 수정 기능 구현 및 코드 전반 수정

--- JavaToDoList/src/server/Server.java
+++ JavaToDoList/src/server/Server.java
@@ -28,17 +28,14 @@ public class Server {
         DBConnection.openConnection();
         
 		final int PORT = 9000;
-		Hashtable<String, Socket> clientHt = new Hashtable<>(); // 접속자를 관리하는 테이블
+		Hashtable<String, Socket> clientHt = new Hashtable<>();
 
 		try {
 			ServerSocket serverSocket = new ServerSocket(PORT);
 			String mainThreadName = Thread.currentThread().getName();
-			/* main thread는 계속 accept()처리를 담당한다 */
 			while (true) {
 				System.out.printf("[서버-%s] Client접속을 기다립니다...\n", mainThreadName);
 				Socket socket = serverSocket.accept();
-
-				/* worker thread는 Client와의 통신을 담당한다. */
 				WorkerThread wt = new WorkerThread(socket, clientHt);
 				wt.start();
 			}
@@ -84,10 +81,6 @@ class WorkerThread extends Thread {
 				 * 수신된 객체가 null일 경우 반복문 종료 + 스레드 실행 종료
 				 */
 				Object packetObj = ois.readObject();
-				
-//				if(packetObj instanceof User)processUserPacket(packetObj);
-//				else if(packetObj instanceof ToDoList || packetObj instanceof Diary) processMenuPacket(packetObj);
-
                 if (packetObj instanceof User) {
                     processUserPacket(packetObj);
                 } else if (packetObj instanceof ToDoList || packetObj instanceof Diary) {
@@ -146,7 +139,6 @@ class WorkerThread extends Thread {
 	}
 
 	private void processUserPacket(Object packetObj) throws Exception {
-		// 클라이언트에 응답을 하기 위한 오브젝트
 		Object ackObj = new Object();
 		
 		cmdObj = (Cmd)packetObj;
@@ -171,17 +163,7 @@ class WorkerThread extends Thread {
 			}
 
 		}  else if(cmdObj.getCmd().equals("로그아웃")) {
-//			String id = packetObj.getString("id");
-			
-//			System.out.printf("<서버-%s> Id=%s 로그아웃 요청 \n", getName(), id);
-			
-			// 관리에서 제외
-//			ht.remove(id);
-//			
-//			ackObj.put("cmd", "QUIT");
-//			ackObj.put("id", id);
 			String ack = ackObj.toString();
-			// 클라이언트한테 전송
 			OutputStream out = this.socket.getOutputStream();
 			PrintWriter pw = new PrintWriter(new OutputStreamWriter(out));
 			pw.println(ack);
